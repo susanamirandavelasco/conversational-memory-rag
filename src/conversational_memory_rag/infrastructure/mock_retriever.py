5fe388aebfc1e46c23ace37fea476aa6170c0901
Susana Miranda <susana.miranda.velasco@gmail.com>
2026-09-10 10:49:07 -0600

    Build Sprint 1 engineering foundation

--- src/conversational_memory_rag/infrastructure/mock_retriever.py
+++ src/conversational_memory_rag/infrastructure/mock_retriever.py
@@ -1,16 +1,12 @@
 from conversational_memory_rag.application.retriever import Retriever
-
+from conversational_memory_rag.domain.conversation_context import ConversationContext
 from conversational_memory_rag.domain.retrieval_result import RetrievalResult
 from conversational_memory_rag.domain.retrieved_chunk import RetrievedChunk
-from conversational_memory_rag.domain.conversation_context import ConversationContext
-
 
 
 class MockRetriever(Retriever):
-
     def retrieve(
-        self,
-        conversation_context: ConversationContext
+        self, conversation_context: ConversationContext, n_results: int = 3
     ) -> RetrievalResult:
 
         query = conversation_context.get_last_user_message().content
@@ -22,8 +18,9 @@ class MockRetriever(Retriever):
                 RetrievedChunk(
                     content="Amazon Bedrock is a funny, interesting, happy and fully managed service... (This is a mock Retriever)",
                     source="bedrock.pdf",
+                    page_range="12",
                     chunk_number=12,
-                    score=0.98
+                    score=0.98,
                 ),
             )
         )
